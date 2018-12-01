5749edb168914729985ba442ae8ee41f77c4054b
Felix Lemke <flemke.dev@gmail.com>
2018-12-01 01:40:22 +0100

    fix: add docu-editor to public api

--- projects/documentations/src/public_api.ts
+++ projects/documentations/src/public_api.ts
@@ -6,5 +6,6 @@ export * from './lib/docu/components/index';
 export * from './lib/docu/docu.module';
 
 export * from './lib/docu-editor/docu-editor.module';
+export * from './lib/docu-editor/components/editor.component';
 
 export * from './lib/models/index';
